7bd45a58d1e7ed606e4cb341df1fbf2273108f13
jeck99 <jeck99@gmail.com>
2022-02-23 19:12:46 +0200

    added comments to make it easy

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
+// creating the server 
 const netServer = require('http').createServer(handler)
+// creating the socket based on the server
 const io = require('socket.io')(netServer);
+// importing the fs module
 const fs = require('fs');
-
-// netServer.on('request', handler);
-function handler (req, res) {
+// request handler function to handle the request event
+function handler(req, res) {
+  // reading the html file
   fs.readFile(__dirname + '/index.html',
-   (err, data)=> {
-    if (err) {
-      return res.end('Error loading index.html');
-    }
-    res.end(data);
-  });
+    (err, data) => {
+      if (err) {
+        return res.end('Error loading index.html');
+      }
+      // sending the html file - the browser will render it
+      res.end(data);
+    });
 }
-
+// listening on to the connection event
 io.on('connection', (socket) => {
   socket.on('message', msg => {
+    // sending the message to all the clients
     io.emit('message', msg);
   });
 });
-
+// listening on to the port 3000
 netServer.listen(3000);
